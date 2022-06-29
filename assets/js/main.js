6f46f3842d074ffad827d9f7fc5301a33d54855a
Liviu Trandafir <liviu@gameco.net>
2022-06-29 17:53:07 +0300

    Add table in two columns

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
 // Load Bootstrap JS
-import bootstrap from 'bootstrap'
+import bootstrap from 'bootstrap';
+import $ from 'jquery';
 
 // Load Styles
 import '../scss/main.scss';
 
-// App code
-console.log(`Hello ${process.env.HELLO}`);
+$(document).ready(function() {
+
+    let elementsColLeft = $('.elements-col-left');
+    let elementsColRight = $('.elements-col-right');
+
+    $.get("https://jsonplaceholder.typicode.com/posts", function(data) {
+        for (let i = 0; i < data.length; i++) {
+            i < (data.length / 2) ? elementsColLeft.append(`<li> ${i + 1} ${data[i].title} </li>`) : elementsColRight.append(`<li> ${i + 1} ${data[i].title} </li>`);
+        }
+    });
+});
